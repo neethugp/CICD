26fbef84024c82bab0c6384d47cf93718ea0a6f3
Neethugp <neethugp2002@gmail.com>
2024-09-23 16:15:45 +0100

    firstCommitforCICD

---
+++ src/test/java/Testcases/SampleTestcase.java
@@ -0,0 +1,36 @@
+package Testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class SampleTestcase {
+
+    public static WebDriver driver = null;
+
+    @BeforeMethod
+    public void launchDriver(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver. manage().window().fullscreen();
+    }
+
+    @Test
+    public void test(){
+        driver.get("https://www.google.com/");
+        System.out.println("Google launched");
+    }
+
+    @AfterMethod
+    public void exit(){
+        driver.quit();
+    }
+
+
+}
